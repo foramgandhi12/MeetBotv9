22b9b2e15d4046bdad6202d163a18248c521cc86
Rutvi Shah <32719043+rutvishah859@users.noreply.github.com>
2022-04-04 14:34:16 -0400

    Manual attempy (does not work)

--- tests/test_automate.py
+++ tests/test_automate.py
@@ -1,10 +1,33 @@
 import unittest
-# from MeetBotv9 import start
+from telegram import Message
+from telegram import Update
+from telegram import User
+from telegram.ext import CallbackContext
+from telegram.ext import Updater
+from datetime import datetime
+from telegram import Chat, Bot
+
+from MeetBotv9 import BOT_TOKEN, USER_ID, help
 from MeetBotv9 import telegram_bot_sendtext
-# from automate import start
+
+update_id_sample = 123456789
+user_sample = User(id=update_id_sample, first_name='test first name', is_bot=True)
+
+message_sample = Message(message_id=1234, date=datetime.now(), chat=Chat(id=int(USER_ID), type='test'), from_user=user_sample)
+update_sample = Update(
+    update_id=update_id_sample,
+    message=message_sample
+)
+updater_sample = Updater(use_context=True, bot=Bot(token=BOT_TOKEN))
+context_sample = CallbackContext(updater_sample.dispatcher)
+
 
 class TestAutomate(unittest.TestCase):
     def test_sendText(self):
-        # pass
         self.assertEquals(telegram_bot_sendtext("test message"), 1)
-        # assert BOT_TOKEN == "5294114162:AAF6XQy-TcAetSjXGfXhDqBqtZx8Ju7MVlA"
+
+    def test_token(self):
+        self.assertEquals(BOT_TOKEN, '5294114162:AAF6XQy-TcAetSjXGfXhDqBqtZx8Ju7MVlA')
+
+    def test_help(self):
+        help(update=update_sample, context=context_sample)
